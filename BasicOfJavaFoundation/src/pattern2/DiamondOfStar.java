505b59ef8c61aca6d0d4c9ac2b55e3e3a9dbd639
rahul <rahul@DESKTOP-HSTRGD7>
2022-08-17 22:48:12 +0530

    Diamond code completed

--- BasicOfJavaFoundation/src/pattern2/DiamondOfStar.java
+++ BasicOfJavaFoundation/src/pattern2/DiamondOfStar.java
@@ -9,11 +9,63 @@
  *   
  */
 package pattern2;
-
+import java.util.Scanner;
 public class DiamondOfStar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int i,num,dec;
+		i=1;
+		while(i<=(n+1)/2 ){
+			int spaces=1;
+			while(spaces<=(n+1)/2-i) {
+				System.out.print(' ');
+				spaces=spaces+1;
+			}		
+			num=1;
+			while(num<=i) {
+				System.out.print('*');
+				num=num+1;
+			}
+			
+			dec=i-1;
+			while(dec>=1) {
+				System.out.print('*');
+				dec=dec-1;
+			}
+			System.out.println();
+			i=i+1;
+		}
+		
+		
+//		i=n-((n+1)/2);
+		i=n/2;
+		while(i>=1) {
+			int spaces=1;
+			while(spaces<n-i-1) {
+				System.out.print(' ');
+				spaces=spaces+1;
+			}		
+			num=1;
+			while(num<=i) {
+				System.out.print('*');
+				num=num+1;
+			}
+			
+			dec=i-1;
+			while(dec>=1) {
+				System.out.print('*');
+				dec=dec-1;
+			}
+			System.out.println();
+			i=i-1;
+		}
+
+
+			
+
 
 	}
 
